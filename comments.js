dd808ed7d2d0a947ace6338c3df97c916f9ca920
JohnCarlS <75965595+JohnCarlSalao@users.noreply.github.com>
2024-09-18 05:06:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,34 @@
-const http = require('http');
+// Create a web server that listens on port 3000.
+// When you visit http://localhost:3000/comments, it should display a list of comments in a bulleted list.
+// The comments should be stored in a separate file called comments.json.
+// The comments should be read from the comments.json file and displayed in a bulleted list.
+// The comments should be displayed in the order they were added to the comments.json file.
 
-// Create a web server
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Welcome to the comments page</h1>');
-    res.end();
-}).listen(3000, () => {
-    console.log('Server is running on port 3000');
-}).on('error', (err) => {
-    console.error('Error:', err);
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/comments') {
+    fs.readFile('comments.json', 'utf8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      } else {
+        const comments = JSON.parse(data);
+        let output = '<ul>';
+        comments.forEach(comment => {
+          output += `<li>${comment}</li>`;
+        });
+        output += '</ul>';
+        res.end(output);
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
