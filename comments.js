37603e77d24a8a1d66adb9313c8e3215d72df318
JohnCarlS <75965595+JohnCarlSalao@users.noreply.github.com>
2024-09-18 05:02:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,14 +2,11 @@ const http = require('http');
 
 // Create a web server
 http.createServer((req, res) => {
-    // Write a response header
     res.writeHead(200, {'Content-Type': 'text/html'});
-
-    // Write a response body
     res.write('<h1>Welcome to the comments page</h1>');
-
-    // End the response
     res.end();
 }).listen(3000, () => {
     console.log('Server is running on port 3000');
+}).on('error', (err) => {
+    console.error('Error:', err);
 });
